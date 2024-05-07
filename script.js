3bcd2a3c4d4fff65637c1b5265d007cdaad6cd0e
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-07 17:05:43 +0300

    step 63

--- script.js
+++ script.js
@@ -18,10 +18,23 @@ function isInvalidInput(str) {
 }
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = (targetInputContainer.querySelectorAll('input[type="text"]').length) + 1 ;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />`;
+    <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"/>`;
+    targetInputContainer.insertAdjacentHTML("beforeend",HTMLString);
 }
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for (const item of list) {
+      const currVal = cleanInputString(item.value);
+      const invalidInputMatch = isInvalidInput(currVal);
+       if (invalidInputMatch) {
+         alert(`Invalid Input: ${invalidInputMatch[0]}`);
+        }
+    }
+}
+
+addEntryButton.addEventListener("click",addEntry);
