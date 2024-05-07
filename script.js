9d98cb26fd95c0b6057c9ed04a74faeea337252a
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-07 14:31:06 +0300

    step 34

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ let isError = false;
 
 
 function cleanInputString(str){
-    console.log("original string: ",str);
-    const regex = /[+-\s]/g;
+    // step 27 for seeing if it works fine -- console.log("original string: ",str);
+    const regex = /[+-\s]/g; //\s boşluk karakteri yapar
     return str.replace(regex, '');
+}
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i; // \d [0-9] karakterlerinin kısaltması 
+    return str.match(regex);
 }
